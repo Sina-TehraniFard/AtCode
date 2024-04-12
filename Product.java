d358ed70d552cd54b2697b752279008c7cb1b584
GT_Tehranifard_Sina <tehranifard.sina@growtech.co.jp>
2024-04-12 18:50:42 +0900

    練習問題2

---
+++ Product.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class Product {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        int result =(a * b)  % 2;
+        if(result == 0) {
+            System.out.println("Evan");
+        } else if (result != 0) {
+            System.out.println();
+        }
+
+    }
+}
