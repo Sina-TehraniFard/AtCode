7bfecdfdd762e81b263350d283925c171ab196db
Sina-TehraniFard <tf.sina.system@gmail.com>
2024-04-30 22:58:22 +0900

    練習問題2 完成品　練習問題１コメント修正

--- Product.java
+++ Product.java
@@ -1,16 +1,27 @@
-import java.util.Stack;
+import java.util.Scanner;
 
+/**
+ * 2024年4月30日
+ *
+ * @author テヘラニファルド スィナ<br>
+ * 問題文参照：<a href="https://atcoder.jp/">AtCode</a><br>
+ * シカのAtCoDeerくんは二つの正整数 a,b を見つけました。<br>
+ * a と b の積が偶数か奇数か判定してください。
+ */
 public class Product {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
+        //標準入力：入力形式（a b）
+        Scanner scanner = new Scanner(System.in);
 
-        int result =(a * b)  % 2;
-        if(result == 0) {
-            System.out.println("Evan");
-        } else if (result != 0) {
-            System.out.println();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        // 積が奇数なら Odd と、 偶数なら Even と出力する。
+        int result = (a * b) % 2;
+        if (result == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
         }
-
     }
 }
